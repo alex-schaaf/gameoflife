a6e6c64326cf3ff67dc63d445a5de2a49c79db69
alex-schaaf <17277800+alex-schaaf@users.noreply.github.com>
2020-10-03 13:06:35 +0200

    ♻ refactor into life module and func

--- lifeserver/life/life.go
+++ lifeserver/life/life.go
@@ -1,16 +1,17 @@
-package main
+package life
 
 import "fmt"
 
-const width = 30
-
 var p, q, r uint8
 
-func main() {
-	initialSate := [width]uint8{}
+func life(width int) [][]uint8 {
+	initialSate := []uint8{}
+	for i := 0; i < width; i++ {
+		initialSate = append(initialSate, 0)
+	}
 	initialSate[width/2] = 1
 
-	states := [][width]uint8{}
+	states := [][]uint8{}
 	states = append(states, initialSate)
 
 	for states[len(states)-1][0] == 0 {
@@ -19,13 +20,13 @@ func main() {
 		newState := evolve(currentState)
 		states = append(states, newState)
 	}
-
+	return states
 }
 
 // Evolve a current state of life using a rule and return the evolved state.
-func evolve(currentState [width]uint8) [width]uint8 {
-	newState := [width]uint8{}
-	for i := 0; i < width; i++ {
+func evolve(currentState []uint8) []uint8 {
+	newState := []uint8{}
+	for i := 0; i < len(currentState); i++ {
 
 		if i > 0 {
 			p = currentState[i-1]
@@ -33,13 +34,13 @@ func evolve(currentState [width]uint8) [width]uint8 {
 
 		q = currentState[i]
 
-		if i < width-1 {
+		if i < len(currentState)-1 {
 			r = currentState[i+1]
 		}
 
 		result := rule30(p, q, r)
 
-		newState[i] = result
+		newState = append(newState, result)
 	}
 	return newState
 }
