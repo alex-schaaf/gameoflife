9928ab5ac5d43de135fa1082c5e3dd87c331f28f
alex-schaaf <17277800+alex-schaaf@users.noreply.github.com>
2020-10-03 12:14:24 +0200

    Refactoring ♻

--- lifeserver/life/life.go
+++ lifeserver/life/life.go
@@ -4,11 +4,11 @@ import "fmt"
 
 const width = 30
 
+var p, q, r uint8
+
 func main() {
 	initialSate := [width]uint8{}
 	initialSate[width/2] = 1
-
-	var p, q, r uint8
 
 	states := [][width]uint8{}
 	states = append(states, initialSate)
@@ -16,24 +16,35 @@ func main() {
 	for states[len(states)-1][0] == 0 {
 		currentState := states[len(states)-1]
 		fmt.Println(currentState)
-		newState := [width]uint8{}
-		for i := 0; i < width; i++ {
-
-			if i > 0 {
-				p = currentState[i-1]
-			}
-
-			q = currentState[i]
-
-			if i < width-1 {
-				r = currentState[i+1]
-			}
-
-			result := p ^ (q | r)
-
-			newState[i] = result
-		}
+		newState := evolve(currentState)
 		states = append(states, newState)
 	}
 
+}
+
+// Evolve a current state of life using a rule and return the evolved state.
+func evolve(currentState [width]uint8) [width]uint8 {
+	newState := [width]uint8{}
+	for i := 0; i < width; i++ {
+
+		if i > 0 {
+			p = currentState[i-1]
+		}
+
+		q = currentState[i]
+
+		if i < width-1 {
+			r = currentState[i+1]
+		}
+
+		result := rule30(p, q, r)
+
+		newState[i] = result
+	}
+	return newState
+}
+
+// rule30: (p, q, r) ↦ p XOR (q OR r)
+func rule30(p uint8, q uint8, r uint8) uint8 {
+	return p ^ (q | r)
 }
